bc9fe664e4eeb06a2777ce26148378b07f65c9f0
Chen-Chun <s110810501@student.nqu.edu.tw>
2020-06-07 16:32:04 +0800

    add

--- homework/hw5/TwoDimensional/MatrixTranspose.js
+++ homework/hw5/TwoDimensional/MatrixTranspose.js
@@ -3,13 +3,11 @@ var a = [[1, 2, 3], [3, 2, 1]];
 var c = [];
 var i,j;
 
-for(i = 0; i < 3; i++)
-{
+for(i = 0; i < 3; i++){
     c[i] = [];
-   for(j = 0; j < 2; j++)
-   {
+   for(j = 0; j < 2; j++){
         c[i][j] = a[j][i];
    }
-   
 }
+
 console.log(c);
